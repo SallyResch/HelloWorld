155e38ea50171ea7e05a4227ec3ba65f6ef43a70
Sally Resch <sallyresch27@gmail.com>
2022-06-29 18:10:35 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {//Body
+
+    public static void main(String[] args){//code block
+        System.out.println("Hello, Sally"); //Statement
+
+
+        int myFirstNumber = (10 + 5) + (2 * 10); //Data type, name, value to the variable
+        int mySecondNumber = 12;
+        int myThirdNumber= myFirstNumber*2;
+        int myTotal= myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
+
+    }
+}
